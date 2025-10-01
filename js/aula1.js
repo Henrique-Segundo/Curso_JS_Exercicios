53dd45bce9987af6df29f083024f2bb9f1999ea7
Henrique-Segundo <henriquesegundo2000@gmail.com>
2025-09-30 22:55:18 -0300

    Aula 2 JS

---
+++ js/aula1.js
@@ -0,0 +1,29 @@
+/*
+Codigo Externo
+*/
+
+document.write("Olá mundo")
+console.log("olá outro mundo")
+//alert("Olá mundo com alerta")
+
+let mensagem = "Minha mensagem"
+let mensagem2 = 'Outra Mensagem'
+let meuPeso = 11.1
+let minhaAltura = 1.11
+let minhaIdade = 10
+let ehDoador = false
+let teste = null
+const PI = 3.1415
+const TAXA = 0.5
+
+teste = "algum conteudo"
+
+document.write("<p>Mensagem " + mensagem + "</p>")
+document.write(`Mensagem2: ${mensagem2}`)
+document.write("<p> Peso: " + meuPeso + "Kg </p>")
+document.write(`Altura: ${minhaAltura}m`)
+document.write("<p>Idade " +minhaIdade+"anos </p>")
+document.write(`Doador? ${ehDoador}`)
+document.write("<p>Teste: " +teste+ "</p>")
+document.write("<p>PI: "+PI+"</p>")
+document.write("<p>TAXA"+ TAXA +"</p>")
